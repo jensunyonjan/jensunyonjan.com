9f57a445df5fe557038d0dd669b569958bdceb1c
jensunyonjan <yonjanjensun11@gmail.com>
2026-08-04 23:20:36 +1000

    Fix config path formatting and wipe cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jensunyonjan.github.io',
-  base: '/://jensunyonjan.com',
+  site: 'https://github.io',
+  base: '/jensunyonjan.com/',
 });
